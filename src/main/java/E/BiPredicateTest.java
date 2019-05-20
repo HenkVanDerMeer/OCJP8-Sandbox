6a7dc4dc06ebdc8004d41794fbef4c9cd6959122
Henk van der Meer <henk.vandermeer@xs4all.nl>
2019-05-20 14:56:51 +0200

    Update BiPredicateTest.java
    WIP

--- src/main/java/E/BiPredicateTest.java
+++ src/main/java/E/BiPredicateTest.java
@@ -6,7 +6,7 @@ public class BiPredicateTest {
     public static void main(String[] args) {
         BiPredicate<String, String> bp1 = (string, prefix) -> string.startsWith(prefix);
         BiPredicate<String, String> bp2 = String::startsWith;
-        System.out.println(bp1.test("JavaScript", "Java"));
-        System.out.println(bp2.test("JavaScript", "Java"));
+        System.out.println(bp1.test("JavaScript", "Java")); // returns true
+        System.out.println(bp2.test("JavaScript", "java")); // returns false
     }
 }
