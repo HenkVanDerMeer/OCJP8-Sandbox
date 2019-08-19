110bb2d0f9665f074b387f410e1de8f86821f544
HenkVanDerMeer <henk.vandermeer@xs4all.nl>
2019-08-19 16:19:53 +0200

    WIP

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -1,10 +1,25 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Tester implements Sprint {
-    @Override
-    public void sprint(String text) {
-        System.out.println("Tussentext: " + text);
+
+    public static void main(String[] args) {
+        IntStream ints = IntStream.rangeClosed(1,10);
+        System.out.println("Stats for ints stream: " + ints.summaryStatistics());
+        Stream<String> animals = Stream.of("lions", "tigers", "bears");
+        System.out.println("Animals:");
+        // 1. Print animals as a list
+        System.out.println("[" + animals.collect(Collectors.joining(", ")) + "]"); // lions, tigers, bears
+        // 2. Print animals as a stream
+//        animals.forEach(System.out::println);
     }
 
     public void tussensprint(String text) {
         System.out.println("Text: " + text);
+    }
+
+    public void sprint(String text) {
+
     }
 }
